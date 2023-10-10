64fc414a606a41b6a92cbea21ab792ee6299e3c0
bit-business <101217799+bit-business@users.noreply.github.com>
2023-10-10 17:03:58 +0200

    1

--- src/Config/skijasi-commerce-theme.php
+++ src/Config/skijasi-commerce-theme.php
@@ -8,9 +8,10 @@ return [
     |
     | This prefix will set the entry point of the skijasi commerce theme.
     |
-    */
+   
     'theme_prefix' => env('MIX_COMMERCE_WEB_ROUTE_PREFIX', 'web'),
-
+     */
+    'theme_prefix' => '',
     /*
     |--------------------------------------------------------------------------
     | Override Controller
